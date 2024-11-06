4c35e042cefbcefb42aac80c08ebc98fa4fe26e1
Julito05 <156558939+Julito05@users.noreply.github.com>
2024-11-06 01:03:55 -0300

    Add files via upload

---
+++ Ciudad.java
@@ -0,0 +1,15 @@
+package aed;
+
+public class Ciudad {
+    int nombre;
+    int ganancia;
+    int perdida;
+    
+
+    public Ciudad(int nombre, int ganancia, int perdida){
+        this.nombre = nombre;
+        this.ganancia = ganancia;
+        this.perdida = perdida;
+    
+    }
+}
