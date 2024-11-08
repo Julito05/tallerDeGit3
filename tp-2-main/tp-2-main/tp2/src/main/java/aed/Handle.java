aeb645403aeb22c3d1af0881bab30031948b5f26
Julito05 <156558939+Julito05@users.noreply.github.com>
2024-11-08 01:00:46 -0300

    Add files via upload

---
+++ tp-2-main/tp-2-main/tp2/src/main/java/aed/Handle.java
@@ -0,0 +1,11 @@
+package aed;
+
+public class Handle {
+    public Traslado traslado;
+    int posicion;
+
+    public Handle(Traslado traslado, int posicion){
+        this.traslado = traslado;
+        this.posicion = posicion;
+    }
+}
